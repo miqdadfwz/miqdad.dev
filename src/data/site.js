859e67311dd1c81803ea73c3c76e235b96dd60ba
miqdadfwz <miqdad.fawwaz@tokopedia.com>
2021-05-07 15:27:24 +0700

    refactor: clean up

---
+++ src/data/site.js
@@ -0,0 +1,10 @@
+module.exports = {
+  title: 'miqdad.dev',
+  description: 'Eleventy project with Webpack and Tailwind CSS',
+  url: 'http://miqdad.dev',
+  twitter: 'https://twitter.com/miqdadfwz',
+  github: 'https://github.com/miqdadfwz',
+  source: 'https://github.com/miqdadfwz/miqdad.dev',
+  mail: 'mailto:miqdad.fawwaz@gmail.com',
+  year: new Date().getFullYear(),
+};
